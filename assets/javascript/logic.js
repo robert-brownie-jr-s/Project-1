237db945ebc37837b77f6875b4f1dc3d398eadbb
tstike44 <you@example.com>
2019-03-29 20:05:12 -0700

    stuck on geting the quesitons to display

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,60 @@
+$(document).ready(function(){
+
+
+
+function quizQuestions () {
+var quizItems = [{
+  question: "How far are you willing to travel?",
+  options: ["5 miles", "10 miles","25 miles"],
+},
+{
+  question: "Do you want to be active?",
+  options: ["yes", "no"],
+
+},
+{
+  question: "Do you want to spend money?",
+  options: ["yes", "no"],
+},
+{
+  question: "How big is you group?",
+  options: ["1", "2" , "3 or more"],
+}] //end of quiz items 
+
+
+ function quizPage() {
+	var quizDiv = $("<div>");
+	quizItems.attr('class', 'quiz-items');
+	quizDiv.append(quizItems.question);
+	quizDiv.append(quizItems.options);
+
+	
+
+ } //end of quizPage function
+ quizPage();
+ $("#submit-home").trigger('click');
+} //end of questions function
+
 $("#submit-home").click(function () {
-	$(".subcontainer").remove();
+  $(".subcontainer").remove();
   console.log('hello')
-  
 
-  var quizDiv = $("<div>");
+ 
+quizPage();
+}) //end of submit function
 
-})
+/*
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA0VKHpsqWkKic4BZ4Sc8ArYp7WdLvQ-Vc",
+  authDomain: "project1-c10f1.firebaseapp.com",
+  databaseURL: "https://project1-c10f1.firebaseio.com/",
+  projectId: "project1-c10f1",
+  storageBucket: "project1-c10f1.appspot.com",
+  messagingSenderId: "696380136176"
+};
+firebase.initializeApp(config);
+*/
 
-var quizItems = [
-  
-]
 
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA0VKHpsqWkKic4BZ4Sc8ArYp7WdLvQ-Vc",
-    authDomain: "project1-c10f1.firebaseapp.com",
-    databaseURL: "https://project1-c10f1.firebaseio.com/",
-    projectId: "project1-c10f1",
-    storageBucket: "project1-c10f1.appspot.com",
-    messagingSenderId: "696380136176"
-  };
-  firebase.initializeApp(config);
-
+}) //end of document ready function
