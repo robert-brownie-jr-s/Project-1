4dd678e75c427144fd8a779d689222d0b005e944
tstike44 <you@example.com>
2019-03-30 17:32:06 -0700

    finished questionnaire page...does not have functionality yet

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -26,29 +26,23 @@ $(document).ready(function(){
 
 //created questions on page
 for (i = 0; i < quizItems.length; i++) {
-  $("#questions-here").append("<h3 id='question-text'>" + quizItems[i].question + "</h3>");
+  $("#questions-here").append("<h3 id='question-text' class='row-fluid'>" + quizItems[i].question + "</h3>");
   
 //created options for each question
   for (j = 0; j < quizItems[i].options.length; j++) {
-    $('#questions-here').append("<input type='radio' class='option-here' name='question-" + i + "' value= " + quizItems[i].options[j] + "' >" + quizItems[i].options[j]);
+    $('#questions-here').append("<div class='option-div inline row-fluid'> <input type='radio' class='option-here ' name='question-" + i + "' value= " + quizItems[i].options[j] + "'> </div>" + quizItems[i].options[j]);
   
 
   } //end of j for loop
 
 } //end of i for loop
 
-//start of new jumbotron
-
-
-
-var cityCard = $(' <div class="card text-white bg-success mb-3" style="max-width: 18rem;">' + 
-'<div class="card-header">City/Zip</div>' +
-'<div class="card-body">' +
+var city = $('<div class="row-fluid city">' +
  '<h5 class="card-title">What City/Zip Code are you planning to be in (Must be in Orange County)</h5>' + 
   '<input id="user-input">' + 
 '</div>')
 
-$(".zip-code").append(cityCard);
+$("#questions-here").append(city);
 
 
 
