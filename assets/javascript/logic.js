f768182b0b08e7d19c12082a79a37e0096a17567
Trevor <tstike44@hotmail.com>
2019-04-02 21:45:14 -0700

    progress on api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -78,6 +78,25 @@ $(document).ready(function () {
 			$(".quiz-container").remove();
 			$("#map").show();
 
+			// START OF API //
+			function start() {
+				// 2. Initialize the JavaScript client library.
+				gapi.client.init({
+					'apiKey': 'AIzaSyCLoFEbhSlB0abkW_Ipic1I18qD7-mtHa0',
+				}).then(function () {
+					// 3. Initialize and make the API request.
+					return gapi.client.request({
+						'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+					})
+				}).then(function (response) {
+					console.log(response.result);
+				}, function (reason) {
+					console.log('Error: ' + reason.result.error.message);
+				});
+			};
+			// 1. Load the JavaScript client library.
+			gapi.load('client', start);
+
 		}) //end of submit function
 
 	}) //end of start function
