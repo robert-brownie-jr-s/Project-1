8e6da708363513ace84cba554383e49450348b47
Trevor <tstike44@hotmail.com>
2019-04-03 11:23:35 -0700

    got the coordinates to be defauted to orange county

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -73,43 +73,15 @@ $(document).ready(function () {
 
 			//set of the results page
 			$(".quiz-container").remove();
-			/*
-				  //this is the map
-				  function initMap() {
-					map = new google.maps.Map(document.getElementById("map"), {
-					  center: {lat: -34.397, lng: 150.644},
-					  zoom: 8
-					});
-				  }
-				  initMap();
 
-			// START OF API //
-			function start() {
-				// 2. Initialize the JavaScript client library.
-				gapi.client.init({
-					'apiKey': 'AIzaSyCLoFEbhSlB0abkW_Ipic1I18qD7-mtHa0',
-				}).then(function () {
-					// 3. Initialize and make the API request.
-					return gapi.client.request({
-						'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
-					})
-				}).then(function (response) {
-					console.log(response.result);
-				}, function (reason) {
-					console.log('Error: ' + reason.result.error.message);
-				});
-			};
-			// 1. Load the JavaScript client library.
-			gapi.load('client', start);
-			*/
 
 			function initMap() {
-				var sydney = new google.maps.LatLng(-33.867, 151.195);
+				var orangeCounty = new google.maps.LatLng(33.7175, -117.8311);
 		
 				infowindow = new google.maps.InfoWindow();
 		
 				map = new google.maps.Map(
-					document.getElementById("map"), {center: sydney, zoom: 15});
+					document.getElementById("map"), {center: orangeCounty, zoom: 10});
 		
 				};
 				initMap();
